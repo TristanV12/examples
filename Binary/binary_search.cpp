5c32e75e579ef407530ae6ff4033f543f7c73065
Tristan Villamil <villamil101@gmail.com>
2018-05-26 03:09:50 -0400

    test suite

--- Binary/binary_search.cpp
+++ Binary/binary_search.cpp
@@ -47,7 +47,7 @@ public:
 			new_node->parent = parent;
 		}
 		// Increment size, return true
-		size++;
+		tree_size++;
 		return true;
 	}
 
@@ -64,20 +64,29 @@ public:
 			tmp_node = locateNode(value);
 			if(tmp_node == NULL){ return false; }
 		}
-		--size;
+
+		// Increment size
+		--tree_size;
+
+		// The node we will be freeing
 		node* real_del;
+
+		// Special case if only left leaf exists (just copy right leaf)
 		if(tmp_node->left == NULL && tmp_node->right != NULL){
 			*tmp_node = *(tmp_node->right);
 			real_del = tmp_node->right;
+		// If no leaves, remove
 		}else if(tmp_node->left == NULL){
 			real_del = tmp_node;
 			if(tmp_node == (tmp_node->parent)->left){ (tmp_node->parent)->left = NULL; }
 			else{ (tmp_node->parent)->right = NULL; }
+		// Otherwise, find largest node to the left of current node
 		}else{
 			real_del = tmp_node->left;
 			while(real_del->right != NULL){
 				real_del = real_del->right;
 			}
+			// Take its value
 			tmp_node->value = real_del->value;
 			if(real_del->left == NULL){
 				(real_del->parent)->right = NULL;
@@ -86,18 +95,21 @@ public:
 				(real_del->left)->parent = real_del->parent;
 			}
 		}
-		// free(real_del);
+		// Garbage collection
+		free(real_del);
 		return true;
 	}
 
+	// contains checks if value exists in the tree
 	bool contains(int value){
 		// If tree is empty, obviously it does not contain the value
 		if(root == NULL){ return false; }
 
 		// But if not...
-		return locateEmptyParent(value) != NULL;
+		return locateEmptyParent(value) == NULL;
 	}
 
+	// maxValue finds the maximum value in the tree
 	int maxValue(){
 		// Corner case
 		if(root == NULL){ return 0; }
@@ -108,16 +120,18 @@ public:
 		return tmp_node->value;
 	}
 
+	// minValue finds the minimum value in the tree
 	int minValue(){
 		// Corner case
 		if(root == NULL){ return 0; }
 		node* tmp_node = root;
 
-		// Find max node
+		// Find min node
 		while(tmp_node->left != NULL){ tmp_node = tmp_node->left; }
 		return tmp_node->value;
 	}
 
+	// toString returns a sorted, readable representation of the tree
 	std::string toString(){
 		if(root == NULL){ return ""; }
 		std::string output = "";
@@ -125,11 +139,20 @@ public:
 		return output;
 	}
 
-private:
-	node* root = NULL;
-	bool rb = false;
-	int size = 0;
+	// size returns the size of the tree
+	int size(){
+		return tree_size;
+	}
 
+// Private variables and methods
+private:
+	// variables
+	node* root = NULL; // root node
+	bool rb = false; // true if red/black enables
+	int tree_size = 0; // number of nodes in the list
+
+	// locateEmptyParent is a helper method for finding the parent where a new node
+	//	should be inserted. Returns NULL if the value exists
 	node* locateEmptyParent(int value){
 		node* curr_node = root;
 		while(true){
@@ -147,6 +170,8 @@ private:
 		}
 	}
 
+	// locateNode by contrast is a helper method for finding the location of a value
+	//	in the tree. Returns NULL if the value DOES NOT exist
 	node* locateNode(int value){
 		node* curr_node = root;
 		while(true){
@@ -164,6 +189,7 @@ private:
 		}
 	}
 
+	// recurseString is a helper recursive function for printing the sorted tree
 	void recurseString(std::string* output, node* root_node){
 		if(root_node->left != NULL){
 			recurseString(output, root_node->left);
@@ -177,19 +203,42 @@ private:
 	}
 };
 
-int main(int argc, char *argv[]){
+void test(){
 	binaryTree b = new binaryTree();
-	b.add(1);
-	b.add(5);
-	b.add(3);
-	b.add(4);
-	std::cout << b.toString();
-	b.remove(3);
-	std::cout << b.toString();
+	std::string str = "";
+	for(int i = 0; i < 30; ++i){
+		b.add(i);
+	}
+	for(int i = 50; i < 100; ++i){
+		b.add(i);
+	}
+	for(int i = 20; i < 60; ++i){
+		b.add(i);
+	}
+	for(int i = 0; i < 100; ++i){
+		if(i != 0){ str += ", "; }
+		str += std::to_string(i);
+	}
+	std::cout << "toString 100 digits test: ";
+	if(b.toString().compare(str) != 0){ std::cout << "FAILED" << std::endl; }
+	else{ std::cout << "SUCCESS" << std::endl; }
 
-	// for (int i = 0; i < 10; ++i){
-	// 	/* code */
-	// }
+	std::cout << "maxValue 100 digits test: ";
+	if(b.maxValue() != 99){ std::cout << "FAILED" << std::endl; }
+	else{ std::cout << "SUCCESS" << std::endl; }
+
+	std::cout << "minValue 100 digits test: ";
+	if(b.minValue() != 0){ std::cout << "FAILED" << std::endl; }
+	else{ std::cout << "SUCCESS" << std::endl; }
+
+	std::cout << "size 100 digits test: ";
+	if(b.size() != 100){ std::cout << "FAILED" << std::endl; }
+	else{ std::cout << "SUCCESS" << std::endl; }
+
+}
+
+int main(int argc, char *argv[]){
+	test();
     
     return 0;
 }
